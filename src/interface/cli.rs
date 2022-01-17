74dccda5bbeda4096f65a91ce6e812ef9e9a56a4
Ben DeMeyer <ben@bendemeyer.com>
2022-01-16 17:18:28 -0800

    Improved board display in CLI

--- src/interface/cli.rs
+++ src/interface/cli.rs
@@ -3,7 +3,7 @@ use std::{collections::HashSet, time::Instant};
 use num_format::{ToFormattedString, Locale};
 use tabled::{Table, Style, Alignment, Modify, Full};
 
-use crate::{game::Game, interface::{arguments::ParsedArgs, shell::InteractiveShell}, rules::{board::{squares::{BoardSquare, get_notation_string_for_square}, fen_board_from_position, Board}, pieces::{PieceType, movement::Move}}, util::{fen::{get_notation_for_piece, FenBoardState}, zobrist::ZobristId}, testing::{perft::PerftRunner, zobrist::ZobristCollisionTester}};
+use crate::{game::Game, interface::{arguments::ParsedArgs, shell::InteractiveShell}, rules::{board::{squares::{BoardSquare, get_notation_string_for_square}, fen_board_from_position, Board}, pieces::{PieceType, movement::Move, Piece}, Color}, util::{fen::FenBoardState, zobrist::ZobristId}, testing::{perft::PerftRunner, zobrist::ZobristCollisionTester}};
 
 use super::arguments::{ArgumentParser, Arguments};
 
@@ -73,20 +73,34 @@ fn get_text_for_move(mov: &Move) -> String {
 }
 
 
+fn get_unicode_piece_symbol(piece: &Piece) -> String {
+    let char_type = std::str::from_utf8(&[0b11101111 as u8, 0b10111000 as u8, 0b10001110 as u8]).unwrap();
+    match (piece.color, piece.piece_type) {
+        (Color::White, PieceType::Pawn)   => format!("{}{}", "♙", char_type), (Color::Black, PieceType::Pawn)   => format!("{}{}", "♟", char_type),
+        (Color::White, PieceType::Knight) => format!("{}{}", "♘", char_type), (Color::Black, PieceType::Knight) => format!("{}{}", "♞", char_type),
+        (Color::White, PieceType::Bishop) => format!("{}{}", "♗", char_type), (Color::Black, PieceType::Bishop) => format!("{}{}", "♝", char_type),
+        (Color::White, PieceType::Rook)   => format!("{}{}", "♖", char_type), (Color::Black, PieceType::Rook)   => format!("{}{}", "♜", char_type),
+        (Color::White, PieceType::Queen)  => format!("{}{}", "♕", char_type), (Color::Black, PieceType::Queen)  => format!("{}{}", "♛", char_type),
+        (Color::White, PieceType::King)   => format!("{}{}", "♔", char_type), (Color::Black, PieceType::King)   => format!("{}{}", "♚", char_type),
+    }
+}
+
+
 fn format_board_for_display(board: &Board) -> Vec<String> {
     let mut result = Vec::new();
-    result.push(String::from("┌───┬───┬───┬───┬───┬───┬───┬───┐"));
-    fen_board_from_position(&board.position).iter().for_each(|row| {
-        result.push(row.iter().fold(String::from("│"), |row_string, square| {
+    result.push(String::from("  ┌────┬────┬────┬────┬────┬────┬────┬────┐"));
+    fen_board_from_position(&board.position).iter().enumerate().for_each(|(index, row)| {
+        result.push(row.iter().fold(format!("{}{}", (7 - index) + 1, " │"), |row_string, square| {
             format!("{} {} │", row_string, match square {
-                Some(piece) => get_notation_for_piece(*piece).to_string(),
-                None => String::from(" "),
+                Some(piece) => get_unicode_piece_symbol(piece),
+                None => String::from("  "),
             })
         }));
-        result.push(String::from("├───┼───┼───┼───┼───┼───┼───┼───┤"));
+        result.push(String::from("  ├────┼────┼────┼────┼────┼────┼────┼────┤"));
     });
     result.pop();
-    result.push(String::from("└───┴───┴───┴───┴───┴───┴───┴───┘"));
+    result.push(String::from("  └────┴────┴────┴────┴────┴────┴────┴────┘"));
+    result.push(String::from("     a    b    c    d    e    f    g    h  "));
     return result;
 }
 
